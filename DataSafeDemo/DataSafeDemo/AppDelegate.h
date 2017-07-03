665e46c16eb72d7f6a5b1e361ded26c9bbf79fcc
liuzhiwei <zhiwei.liu@zhenai.com>
2017-07-03 11:20:29 +0800

    增加文件

---
+++ DataSafeDemo/DataSafeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataSafeDemo
+//
+//  Created by 刘志伟 on 2017/06/28
+//  Copyright © 2017年 刘志伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
